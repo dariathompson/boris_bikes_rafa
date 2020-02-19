6e4b510bd8185e61b34d278194350beee492c540
dariathompson <daria.kambalina@gmail.com>
2020-02-19 14:49:47 +0000

    Back to the unit - passed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,3 +1,3 @@
-require 'practice'
+require 'bike'
 describe "Bike" do
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,4 @@
-require 'practice'
-# describe "describe DockingStation" do
-#   it 'has the name DockingStation' do
-#     expect(DockingStation).to eq DockingStation
-#   end
-  RSpec.describe DockingStation do
+require 'docking_station'
+describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
-    # expect(DockingStation).to respond_to(:release_bike)
-  end
-  # it 'working method' do
-  #   expect(bike.working?).to eq true
-  # end
+end
