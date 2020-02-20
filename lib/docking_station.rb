c2c0a92b1af14b13c062780ee3eb7ba37e162f41
dariathompson <daria.kambalina@gmail.com>
2020-02-20 09:30:31 +0000

    Raising exceptions

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,11 +3,11 @@ class DockingStation
  attr_reader :bike
     def dock(bike)
       @bike = bike
-
     end
 
     def release_bike
-       Bike.new
+        fail "There are no bikes available" unless @bike
+        @bike
     end
 end
 
@@ -17,8 +17,3 @@ class Bike
   end
 end
 
-
-bike = Bike.new
-station = DockingStation.new
-p station.dock(bike)
-p station.bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,14 +2,18 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
-  it "gets a bike and bike is working" do
-    station = DockingStation.new
-    bike = station.release_bike
+  it "releases working bike" do
+    bike = Bike.new
     expect(bike.working?).to eq true
   end
-   it "docks a bike in the docking station" do
-     bike = Bike.new
-     station = DockingStation.new
-     expect(station.dock(bike)).to eq bike
-   end
+
+  it "docks a bike in the docking station" do
+    bike = Bike.new
+    station = DockingStation.new
+    expect(station.dock(bike)).to eq bike
+  end
+
+  it "raises an error" do
+    expect { raise StandardError }.to raise_error
+  end
 end
