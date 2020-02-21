51d154ed39777794961631cac7536997ac73644f
Student <student@Makerss-MacBook-Air.local>
2020-02-21 09:46:05 +0000

    test with my group

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,16 @@
 
 class DockingStation
  attr_reader :bike
+
     def dock(bike)
       @bike = bike
+
     end
 
     def release_bike
       fail "There are no bikes available" unless @bike
       @bike
-    end
+      end
 end
 
 class Bike
@@ -16,4 +18,3 @@ class Bike
     true
   end
 end
-
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,4 +16,16 @@ describe DockingStation do
   it "raises an error" do
     expect { subject.release_bike }.to raise_error "There are no bikes available"
   end
+  it "raises an error when full" do
+    station = DockingStation.new
+
+    expect { station.dock(0)}.to raise_error "Dock is full you cannot dock any more bikes"
+  end
+    describe '#dock' do
+   it 'raises and error when full' do
+     subject.dock(Bike.new)
+     expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+   end
+ end 
+
 end
