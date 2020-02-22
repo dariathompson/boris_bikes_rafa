1b679933fbc34790a5b41b93c3175e75edcc6402
Student <student@Makerss-MacBook-Air.local>
2020-02-22 08:16:46 +0000

    Step 14 finished

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,22 @@
 
 class DockingStation
- attr_reader :bike
+ attr_reader :bike_container
+
+ def initialize
+   @bike_container = []
+ end
 
     def dock(bike)
-      fail 'Docking station full' if @bike
-      @bike = bike
+      fail 'Docking station full' if @bike_container.count >=20
+      @bike_container << bike
+      @bike_container[0]
+
     end
 
 
     def release_bike
-      fail "There are no bikes available" unless @bike
-      @bike
+      fail "There are no bikes available" if @bike_container.empty?
+      @bike_container.pop
       end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,16 +10,20 @@ describe DockingStation do
   it "docks a bike in the docking station" do
     bike = Bike.new
     station = DockingStation.new
-    expect(station.dock(bike)).to eq bike
+    expect(station.dock(bike)).to eq bike 
   end
 
   it "raises an error" do
     expect { subject.release_bike }.to raise_error "There are no bikes available"
+
   end
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+
+      20.times {subject.dock(Bike.new)}
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+
     end
+
   end
 end
